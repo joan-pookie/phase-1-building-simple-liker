ea6b9007e84cdb6595fbf306ef0d74a9b6a43894
Joan Chepkoech <joan92311@gmail.com>
2025-07-01 04:40:19 +0300

    complete-phase-1-building-simple-liker

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const modal = document.getElementById('modal');
+const modalMessage = document.getElementById('modal-message');
+modal.classList.add('hidden'); // Ensure modal is hidden on page load
+const hearts = document.querySelectorAll('.like-glyph');
+
+hearts.forEach(heart => {
+  heart.addEventListener('click', () => {
+    if (heart.innerText === EMPTY_HEART) {
+      mimicServerCall()
+        .then(() => {
+          heart.innerText = FULL_HEART;
+          heart.classList.add('activated-heart');
+        })
+        .catch(error => {
+          modalMessage.innerText = error;
+          modal.classList.remove('hidden');
+          setTimeout(() => {
+            modal.classList.add('hidden');
+          }, 3000);
+        });
+    } else {
+      heart.innerText = EMPTY_HEART;
+      heart.classList.remove('activated-heart');
+    }
+  });
+});
 
 // Your JavaScript code goes here!
 
